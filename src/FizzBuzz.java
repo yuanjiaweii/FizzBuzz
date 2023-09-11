15537ff51184d928e1dab0f77ffc70e306a56789
yuanjiaweii <vicki.yuan@mail.utoronto.ca>
2023-09-11 19:56:26 -0400

    t

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -8,35 +8,30 @@ class FizzBuzz {
 
         while (i < 100) {
 
-            i = dofizzbuzz(i);
+            // Find out which numbers divide i.
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            // Print our appropriate result.
+            if (divisibleBy3 && divisibleBy5) {
+
+                System.out.println("Fizz Buzz");
+
+            } else if (divisibleBy3) {
+
+                System.out.println("Fizz");
+
+            } else if (divisibleBy5) {
+
+                System.out.println("Buzz");
+
+            } else {
+
+                System.out.println(i);
+
+
+            }
+            i++;
         }
-    }
-
-    private static int dofizzbuzz(int i) {
-        // Find out which numbers divide i.
-        boolean divisibleBy3 = i % 3 == 0;
-        boolean divisibleBy5 = i % 5 == 0;
-
-        // Print our appropriate result.
-        if (divisibleBy3 && divisibleBy5) {
-
-            System.out.println("Fizz Buzz");
-
-        } else if (divisibleBy3) {
-
-            System.out.println("Fizz");
-
-        } else if (divisibleBy5) {
-
-            System.out.println("Buzz");
-
-        } else {
-
-            System.out.println(i);
-
-
-        }
-        i++;
-        return i;
     }
 }
