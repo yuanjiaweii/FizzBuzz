eae8aa769eb7abba681d3c68e4f9385207ed863e
yuanjiaweii <vicki.yuan@mail.utoronto.ca>
2023-09-11 20:09:55 -0400

    t

--- src/FizzBuzz.java
+++ src/FizzBuzz.java
@@ -8,11 +8,11 @@ class FizzBuzz {
 
         while (i < 100) {
 
-            i = dofizzbuzz(i);
+            i = doFizzBuzz(i);
         }
     }
 
-    private static int dofizzbuzz(int i) {
+    static int doFizzBuzz(int i) {
         // Find out which numbers divide i.
         boolean divisibleBy3 = i % 3 == 0;
         boolean divisibleBy5 = i % 5 == 0;
